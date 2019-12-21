bbcd62d19f2665b652ec47a670a7890de819408f
D4ryl3ne1 <Darylehen02@gmail.com>
2019-12-21 12:54:58 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+//Check that my server is up and running 
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//create a GET route
+app.get('/', (req, res) => {
+    res.send({ express: 'Express backend is connected to react'});
+});
