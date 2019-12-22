2d0805037c8316b8f2de26af13f3f527a4e2c7d1
D4ryl3ne1 <Darylehen02@gmail.com>
2019-12-21 20:39:17 -0500

    Created Express server and set up routes

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express')
+require('./services/passport')
+const authRoutes = require('./routes/authRoutes')
 const app = express()
+authRoutes(app)
 const port = process.env.PORT || 5000
-
 //Check that my server is up and running 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-//create a GET route
-app.get('/', (req, res) => {
-    res.send({ express: 'Express backend is connected to react'});
-});
+app.listen(port, () => console.log(`Listening on port ${port}`))
