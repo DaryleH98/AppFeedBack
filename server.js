19d86b104318635e79df2a0c9ab7c345cf880efd
D4ryl3ne1 <Darylehen02@gmail.com>
2020-01-01 17:47:21 -0500

    Added mongoose dependency and wired it to app

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 require('./services/passport')
 const authRoutes = require('./routes/authRoutes')
 const app = express()
@@ -6,3 +8,4 @@ authRoutes(app)
 const port = process.env.PORT || 5000
 //Check that my server is up and running 
 app.listen(port, () => console.log(`Listening on port ${port}`))
+mongoose.connect(keys.mongoURI);
